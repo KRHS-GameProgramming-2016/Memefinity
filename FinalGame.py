613ea927b043ef118125c64ddb65b6330b8f1beb
notassigned <zapman44@gmail.com>
2016-12-05 14:25:03 -0500

    Added FinalGame.py

---
+++ FinalGame.py
@@ -0,0 +1,19 @@
+import pygame, sys, math, random
+from Meme import *
+from Bossmeme import *
+from Wall import *
+from Level import *
+from Player import *
+from Memefinity import *
+pygame.init()
+
+#Hello b0ss, this file shall be the final game, or a script, kinda, whatever.
+#Reading the file top to bottom should be the game from start to finish.
+
+#Level 1 goes here
+
+#cutscene
+
+#Level 2 goes here
+
+#see what I mean?
