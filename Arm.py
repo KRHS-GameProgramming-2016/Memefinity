bcbbc28b55b2ada9c3b35d0e469f0f502aa2b3e6
Person21 <gunnar.andersson@kearsarge.org>
2016-12-06 11:53:37 -0500

    Arm is functional

--- Arm.py
+++ Arm.py
@@ -1,17 +1,27 @@
 import pygame, sys, math
 
 class Arm():
-    def _init_(self, screensize, player):
+    def __init__(self, screensize, player):
         self.player = player
         self.baseImage = pygame.image.load("rsc/ball/PlayerArm.png")
+        self.baseImage = pygame.transform.scale(self.baseImage, [100,100])
         self.image = self.baseImage
         self.rect = self.image.get_rect()
+        self.angle =0
     
-    def aim(self, mousePos):           #http://pygame.org/wiki/RotateCenter?parent=
+    def aim(self, mousePos):           #https://github.com/scscorley/jellitubby-attack/blob/master/Vacuum.py
         """rotate an image while keeping its center and size"""
-        self.angle = 0 #trig from mousePos
+        mousePosPlayerX = mousePos[0] - self.rect.center[0]
+        mousePosPlayerY = mousePos[1] - self.rect.center[1]
+        self.angle = ((math.atan2(mousePosPlayerY, mousePosPlayerX))/math.pi)*180
+        self.angle = -self.angle
+        
         rot_image = pygame.transform.rotate(self.baseImage, self.angle)
-        rot_rect = orig_rect.copy()
+        rot_rect = self.rect.copy()
         rot_rect.center = rot_image.get_rect().center
-        rot_image = rot_image.subsurface(rot_rect).copy()
+        rot_image = rot_image.subsurface(rot_rect)
         self.image = rot_image
+    
+    def update(self):
+        self.rect.center = [self.player.rect.x+20, 
+                            self.player.rect.y+50]
