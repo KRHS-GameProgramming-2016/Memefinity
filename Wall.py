5fd06ca2ca26e0125f0db07fe745de431e865cae
notassigned <zapman44@gmail.com>
2016-12-11 13:02:39 -0500

    added ground blocks with stolen textures untill leland stops being lazy

--- Wall.py
+++ Wall.py
@@ -22,4 +22,16 @@ class Wall_5x5(pygame.sprite.Sprite):
     def shiftX(self, amount):
         self.rect.x += amount           
 
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, pos=[0,0], size=None):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = pygame.image.load("rsc/wall/ground.png")
+        if size:
+            self.image = pygame.transform.scale(self.image, [size,size])
+        self.rect = self.image.get_rect(center = pos)
+        
+    def shiftX(self, amount):
+        self.rect.x += amount
+
+
 #
