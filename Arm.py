266363c0b84e5dee9191e00f44bdc1be90d6c2ca
notassigned <zapman44@gmail.com>
2016-12-19 14:34:17 -0500

    WORK IN PROGRESS

--- Arm.py
+++ Arm.py
@@ -1,12 +1,17 @@
 import pygame, sys, math
+from Player import *
 
 class Arm(pygame.sprite.Sprite):
     def __init__(self, player):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.player = player
-        self.baseImage = pygame.image.load("rsc/ball/PlayerArm.png")
-        self.baseImage = pygame.transform.scale(self.baseImage, [100,100])
-        self.image = self.baseImage
+        self.runRight = pygame.image.load("rsc/ball/PlayerArm.png")
+        self.runLeft = pygame.image.load("rsc/ball/PlayerArmLeft.png")
+        self.restRight = [pygame.transform.scale(pygame.image.load("rsc/ball/PlayerArm.png"), [100,100])]
+        self.restLeft = [pygame.transform.scale(pygame.image.load("rsc/ball/PlayerArmLeft.png"), [100,100])]
+        self.images = self.restRight
+        self.state = "rest right"
+        self.prevState = "rest right"
         self.rect = self.image.get_rect()
         self.angle =0
     
@@ -27,4 +32,17 @@ class Arm(pygame.sprite.Sprite):
         self.rect.center = [self.player.rect.x+46, 
                             self.player.rect.y+38]
 
-#
+        if self.prevState != self.state:
+            self.prevState = self.state
+            if self.player.state == "run right":
+                self.images = self.runRight
+                self.rect.center = [self.player.rect.x+37, 
+                self.player.rect.y+38]
+            if self.player.state == "run left":
+                self.images = self.runLeft
+                self.rect.center = [self.player.rect.x+51, 
+                self.player.rect.y+38]
+            if self.player.state == "rest right":
+                self.images = self.restRight
+            if self.player.state == "rest left":
+                self.images = self.restLeft
