fb6cd3a849e39e1d103873def22abd51e880a499
Person21 <gunnar.andersson@kearsarge.org>
2017-02-16 12:36:12 -0500

    nothing changed

--- BetterBossMeme.py
+++ BetterBossMeme.py
@@ -4,9 +4,9 @@ import pygame, sys, math
 
 
 class BossMeme(pygame.sprite.Sprite):
-    def __init__(self, pos=[0,0], size=None):
+    def __init__(self, pos=[0,0], size=100):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.image = pygame.image.load("rsc/ball/Health.png")
+        self.image = pygame.image.load("rsc/ball/ball.png")
         if size:
             self.image = pygame.transform.scale(self.image, [size,size])
         self.rect = self.image.get_rect(center = pos)
