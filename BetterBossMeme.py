c1b96a76602b94ae56074f5f3d425de6640809f5
Person21 <gunnar.andersson@kearsarge.org>
2017-03-09 11:25:36 -0500

    Trump is now giant

--- BetterBossMeme.py
+++ BetterBossMeme.py
@@ -8,7 +8,7 @@ class BossMeme(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = pygame.image.load("rsc/ball/ball.png")
         if size:
-            self.image = pygame.transform.scale(self.image, [size,size])
+            self.image = pygame.transform.scale(self.image, [size*10,size*10])
         self.rect = self.image.get_rect(center = pos)
         
     def shiftX(self, amount):
